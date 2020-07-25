c5e21f87ecb96b90f6830ff6215efd02dd7e9b51
Charles Cunningham <c.a.cunningham6@gmail.com>
2020-07-25 16:53:34 +0200

    feat(types): define required types

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+export {
+  IKeyRefArgs,
+  IVaultedKeyProvider,
+  ICryptoProvider,
+  PublicKeyInfo,
+  EncryptedWalletUtils,
+  CryptoUtils
+} from './types'
+export { SoftwareKeyProvider } from './softwareKeyProvider'
+export { getCryptoProvider } from './cryptoProvider'
---
+++ src/types.ts
@@ -0,0 +1,106 @@
+
+export interface IKeyRefArgs {
+  encryptionPass: string,
+  keyRef: string,
+};
+
+export const enum KeyTypes {
+  jwsVerificationKey2020 = 'JwsVerificationKey2020',
+  ecdsaSecp256k1VerificationKey2019 = 'EcdsaSecp256k1VerificationKey2019',
+  ed25519VerificationKey2018 = 'Ed25519VerificationKey2018',
+  gpgVerificationKey2020 = 'GpgVerificationKey2020',
+  rsaVerificationKey2018 = 'RsaVerificationKey2018',
+  x25519KeyAgreementKey2019 = 'X25519KeyAgreementKey2019',
+  schnorrSecp256k1VerificationKey2019 = 'SchnorrSecp256k1VerificationKey2019',
+  ecdsaSecp256k1RecoveryMethod2020 = 'EcdsaSecp256k1RecoveryMethod2020',
+};
+
+export interface IVaultedKeyProvider {
+  getPubKey: (refArgs: IKeyRefArgs) => Promise<PublicKeyInfo>,
+  getPubKeys: (pass: string) => Promise<PublicKeyInfo[]>,
+  sign: (refArgs: IKeyRefArgs, data: Buffer) => Promise<Buffer>,
+  decrypt: (refArgs: IKeyRefArgs, data: Buffer) => Promise<Buffer>,
+};
+
+export interface ICryptoProvider {
+  verify: (pkInfo: PublicKeyInfo, data: Buffer, sig: Buffer) => Promise<boolean>,
+  encrypt: (pkInfo: PublicKeyInfo, toEncrypt: Buffer, aad?: Buffer) => Promise<Buffer>,
+  getRandom: (nr: number) => Promise<Buffer>,
+};
+
+export interface PublicKeyInfo {
+  "@context": string[],
+  id: string,
+  type: KeyTypes,
+  public_key: string
+};
+
+export interface EncryptedWalletUtils {
+  newWallet: (
+    id: string,
+    pass: string
+ ) => Promise<string>,
+  
+  changePass: (
+    encryptedWallet: string,
+    id: string,
+    oldPass: string,
+    newPass: string
+ ) => Promise<string>,
+  
+  newKey: (
+    encryptedWallet: string,
+    id: string,
+    pass: string,
+    keyType: string,
+    controller?: string
+ ) => Promise<string>,
+  
+  getKey: (
+    encryptedWallet: string,
+    id: string,
+    pass: string,
+    key_ref: string
+ ) => Promise<string>,
+
+  getKeys: (
+    encryptedWallet: string,
+    id: string,
+    pass: string
+ ) => Promise<string>,
+  
+  sign: (
+    encryptedWallet: string,
+    id: string,
+    pass: string,
+    data: string,
+    key_ref: string
+ ) => Promise<string>,
+
+  decrypt: (
+    encryptedWallet: string,
+    id: string,
+    pass: string,
+    data: string,
+    key_ref: string,
+    aad?: string
+ ) => Promise<string>,
+};
+
+export interface CryptoUtils {
+  getRandom: (
+    len: number
+  ) => Promise<string>,
+
+  verify: (
+    pkInfo: string,
+    data: string,
+    sig: string
+  ) => Promise<boolean>,
+
+  encrypt: (
+    pkInfo: string,
+    to_encrypt: string,
+    aad?: string
+  ) => Promise<string>
+};
